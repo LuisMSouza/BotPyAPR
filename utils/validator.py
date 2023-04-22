13244e84faebed9c19e51c1c3bc55b0f0f5ed6df
LuisMSouza <l.miguel-souza@hotmail.com>
2023-04-22 00:04:53 -0300

    Added checks through regex's

--- utils/validator.py
+++ utils/validator.py
@@ -1,7 +1,13 @@
 import re
 
-def validate_email():
-    ...
 
-def validate_cpf():
-    ...
+def validate_email(email: str) -> bool:
+    regExp = re.compile(
+        r'^([\w\.\-]+)\@([a-z]+)\.([a-z]+\.)?(com|br|com\.br)$', flags=re.I)
+    return True if regExp.match(email) else False
+
+
+def validate_cpf(cpf: int | str) -> bool:
+    regExp = re.compile(
+        r'^(\d{3}\.?\d{3}\.?\d{3}-?\d{2})$')
+    return True if regExp.match(f"{cpf}") else False
